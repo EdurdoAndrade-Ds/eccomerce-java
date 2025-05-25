9763264d7befd8dc71d3dbf92faadee709485394
EduardoAndrade <game4edu5@gmail.com>
2025-05-25 13:39:26 -0300

    error corrected spring boot

--- ecommerce/src/main/java/com/ecommerce/ecommerce/repository/ClienteRepository.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/repository/ClienteRepository.java
@@ -1,8 +1,9 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository {
     List<Cliente> findByEmailContaining(String email);
 }
